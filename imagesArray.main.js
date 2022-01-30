ffaaffb45aa0d7c224c2778339b4c2a8816f21d3
DragostinH <dragostin.hristov@gmail.com>
2022-01-30 18:56:20 +0200

    created Menu tab and added the 4 buttons to switch between menus

--- imagesArray.main.js
+++ imagesArray.main.js
@@ -16,95 +16,14 @@
   \****************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ getImagesArray)\n/* harmony export */ });\n/* harmony import */ var _img_Menu_cooking_sushi1_jpg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./img/Menu/cooking-sushi1.jpg */ \"./src/img/Menu/cooking-sushi1.jpg\");\n/* harmony import */ var _img_Menu_cooking_sushi2_jpg__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./img/Menu/cooking-sushi2.jpg */ \"./src/img/Menu/cooking-sushi2.jpg\");\n/* harmony import */ var _img_Menu_cooking_sushi3_jpg__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./img/Menu/cooking-sushi3.jpg */ \"./src/img/Menu/cooking-sushi3.jpg\");\n/* harmony import */ var _img_Menu_cooking_ramen1_jpg__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./img/Menu/cooking-ramen1.jpg */ \"./src/img/Menu/cooking-ramen1.jpg\");\n/* harmony import */ var _img_Menu_cooking_ramen2_jpg__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./img/Menu/cooking-ramen2.jpg */ \"./src/img/Menu/cooking-ramen2.jpg\");\n/* harmony import */ var _img_Menu_cooking_ramen3_jpg__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./img/Menu/cooking-ramen3.jpg */ \"./src/img/Menu/cooking-ramen3.jpg\");\n\n\n\n\n\n\n\n\nfunction getImagesArray() {\n\n    const cookingSushi1 = new Image();\n    cookingSushi1.src = _img_Menu_cooking_sushi1_jpg__WEBPACK_IMPORTED_MODULE_0__;\n\n    const cookingSushi2 = new Image();\n    cookingSushi2.src = _img_Menu_cooking_sushi2_jpg__WEBPACK_IMPORTED_MODULE_1__;\n\n    const cookingSushi3 = new Image();\n    cookingSushi3.src = _img_Menu_cooking_sushi3_jpg__WEBPACK_IMPORTED_MODULE_2__;\n\n    const cookingRamen1 = new Image();\n    cookingRamen1.src = _img_Menu_cooking_ramen1_jpg__WEBPACK_IMPORTED_MODULE_3__;\n\n    const cookingRamen2 = new Image();\n    cookingRamen2.src = _img_Menu_cooking_ramen2_jpg__WEBPACK_IMPORTED_MODULE_4__;\n\n    const cookingRamen3 = new Image();\n    cookingRamen3.src = _img_Menu_cooking_ramen3_jpg__WEBPACK_IMPORTED_MODULE_5__;\n\n    const images = [cookingSushi1.src, cookingSushi2.src, cookingSushi3.src, cookingRamen1.src, cookingRamen2.src, cookingRamen3.src];\n\n    return images;\n\n}\n\n//# sourceURL=webpack://repos/./src/imagesArray.js?");
-
-/***/ }),
-
-/***/ "./src/img/Menu/cooking-ramen1.jpg":
-/*!*****************************************!*\
-  !*** ./src/img/Menu/cooking-ramen1.jpg ***!
-  \*****************************************/
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
-
-eval("module.exports = __webpack_require__.p + \"0b097f0f103c2c57e2e8.jpg\";\n\n//# sourceURL=webpack://repos/./src/img/Menu/cooking-ramen1.jpg?");
-
-/***/ }),
-
-/***/ "./src/img/Menu/cooking-ramen2.jpg":
-/*!*****************************************!*\
-  !*** ./src/img/Menu/cooking-ramen2.jpg ***!
-  \*****************************************/
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
-
-eval("module.exports = __webpack_require__.p + \"93bef2a257b0474fddc3.jpg\";\n\n//# sourceURL=webpack://repos/./src/img/Menu/cooking-ramen2.jpg?");
-
-/***/ }),
-
-/***/ "./src/img/Menu/cooking-ramen3.jpg":
-/*!*****************************************!*\
-  !*** ./src/img/Menu/cooking-ramen3.jpg ***!
-  \*****************************************/
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
-
-eval("module.exports = __webpack_require__.p + \"cb29d6233df85d38542b.jpg\";\n\n//# sourceURL=webpack://repos/./src/img/Menu/cooking-ramen3.jpg?");
-
-/***/ }),
-
-/***/ "./src/img/Menu/cooking-sushi1.jpg":
-/*!*****************************************!*\
-  !*** ./src/img/Menu/cooking-sushi1.jpg ***!
-  \*****************************************/
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
-
-eval("module.exports = __webpack_require__.p + \"bc4d8801d339d147ad37.jpg\";\n\n//# sourceURL=webpack://repos/./src/img/Menu/cooking-sushi1.jpg?");
-
-/***/ }),
-
-/***/ "./src/img/Menu/cooking-sushi2.jpg":
-/*!*****************************************!*\
-  !*** ./src/img/Menu/cooking-sushi2.jpg ***!
-  \*****************************************/
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
-
-eval("module.exports = __webpack_require__.p + \"f39afd3154a70ec1d229.jpg\";\n\n//# sourceURL=webpack://repos/./src/img/Menu/cooking-sushi2.jpg?");
-
-/***/ }),
-
-/***/ "./src/img/Menu/cooking-sushi3.jpg":
-/*!*****************************************!*\
-  !*** ./src/img/Menu/cooking-sushi3.jpg ***!
-  \*****************************************/
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
-
-eval("module.exports = __webpack_require__.p + \"9fd733d4e2937a5d4d38.jpg\";\n\n//# sourceURL=webpack://repos/./src/img/Menu/cooking-sushi3.jpg?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ getImagesArray)\n/* harmony export */ });\n\nfunction getImagesArray(...images) {\n    return images;\n}\n\n//# sourceURL=webpack://repos/./src/imagesArray.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
 /******/ 	
 /************************************************************************/
 /******/ 	/* webpack/runtime/define property getters */
@@ -117,18 +36,6 @@ eval("module.exports = __webpack_require__.p + \"9fd733d4e2937a5d4d38.jpg\";\n\n
 /******/ 				}
 /******/ 			}
 /******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/global */
-/******/ 	(() => {
-/******/ 		__webpack_require__.g = (function() {
-/******/ 			if (typeof globalThis === 'object') return globalThis;
-/******/ 			try {
-/******/ 				return this || new Function('return this')();
-/******/ 			} catch (e) {
-/******/ 				if (typeof window === 'object') return window;
-/******/ 			}
-/******/ 		})();
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
@@ -147,32 +54,13 @@ eval("module.exports = __webpack_require__.p + \"9fd733d4e2937a5d4d38.jpg\";\n\n
 /******/ 		};
 /******/ 	})();
 /******/ 	
-/******/ 	/* webpack/runtime/publicPath */
-/******/ 	(() => {
-/******/ 		var scriptUrl;
-/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
-/******/ 		var document = __webpack_require__.g.document;
-/******/ 		if (!scriptUrl && document) {
-/******/ 			if (document.currentScript)
-/******/ 				scriptUrl = document.currentScript.src
-/******/ 			if (!scriptUrl) {
-/******/ 				var scripts = document.getElementsByTagName("script");
-/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
-/******/ 			}
-/******/ 		}
-/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
-/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
-/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
-/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
-/******/ 		__webpack_require__.p = scriptUrl;
-/******/ 	})();
-/******/ 	
 /************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./src/imagesArray.js");
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/imagesArray.js"](0, __webpack_exports__, __webpack_require__);
 /******/ 	
 /******/ })()
 ;
